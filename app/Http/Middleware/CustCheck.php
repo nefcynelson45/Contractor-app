702b0591e51a358fbf6a64f786d4fa0aae95f775
nefcynelson45 <nefcynelson@gmail.com>
2021-05-24 18:22:43 +0530

    final commit

--- app/Http/Middleware/CustCheck.php
+++ app/Http/Middleware/CustCheck.php
@@ -17,11 +17,11 @@ class CustCheck
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!session()->has('LoggedUser') && ($request->path() !='auth/login' && $request->path()!='auth/reg')){
-            return redirect('auth/login')->with('fail','You Must be Logged In');
+        if(!session()->has('LoggedUser') && ($request->path() !='/auth/login' && $request->path()!='/auth/reg')){
+            return redirect('/auth/login')->with('fail','You Must be Logged In');
         }
 
-        if(session()->has('LoggedUser')  && ($request->path()=='auth/login' || $request->path()!='auth/reg')){
+        if(session()->has('LoggedUser')  && ($request->path()=='/auth/login' || $request->path()!='/auth/reg')){
             return back();
         }
         return $next($request)->header('Cache-Control','no-cache,no-store,max-age=0, must-revalidate')
