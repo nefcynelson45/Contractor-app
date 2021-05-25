a1840556abf1d34ef51b5b86f29702a67e366423
nefcynelson45 <nefcynelson@gmail.com>
2021-05-25 14:40:49 +0530

    final commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -127,6 +127,15 @@ margin-left: 4px;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script type="text/javascript">
+    $.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+    </script>
     <title>DAVIDSON'S GENERAL CONTRACTORS</title>
 </head>
 
